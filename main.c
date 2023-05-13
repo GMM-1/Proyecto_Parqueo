6a072613f05d4de9d285582f5d07c569ecd245b3
GMM-1 <and21499@uvg.edu.gt>
2023-05-12 22:32:58 -0600

    sensores listo
    listo 4 sensores con dos leds cada uno con funcionamiento correcto, proximo paso: transmicion de datos UART

--- main.c
+++ main.c
@@ -16,29 +16,22 @@
 int main(void) {
     uint8_t i; // Variable para iterar a través de los espacios de estacionamiento
     uint32_t parkingStatus; // Variable para almacenar el estado actual de un espacio de estacionamiento
-    uint8_t availableSpaces = 4, prevAvailableSpaces = 4; // Inicializa variables para contar los espacios disponibles y almacenar el valor previo
+    uint8_t availableSpaces = 4; // Inicializa variable para contar los espacios disponibles
     uint32_t prevParkingStatus[4] = {0, 0, 0, 0}; // Almacena el estado anterior de cada espacio de estacionamiento
 
     // Configurar el reloj del sistema a 40 MHz usando el PLL
     SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
-    // Habilitar los periféricos GPIOB, GPIOF, UART0 y GPIOA
+    // Habilitar los periféricos GPIOB, GPIOF y GPIOD
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
-    // Configurar los pines PB0 - PB3 como entradas y PF1 - PF4 como salidas
+    // Configurar los pines PB0 - PB3 como entradas y PF1 - PF4 y PD0 - PD3 como salidas
     GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4);
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 
-    // Configurar UART0 con los pines PA0 y PA1
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    // Configurar UART0 con una velocidad de 115200 baudios, 8 bits de datos, 1 bit de parada y sin paridad
-    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
     while (1) {
         // Iterar a través de los 4 espacios de estacionamiento
         for (i = 0; i < 4; i++) {
@@ -60,17 +53,23 @@ int main(void) {
             }
 
             // Enciende el LED rojo si el espacio está ocupado y el LED verde si está desocupado
-            if (parkingStatus) {
-                GPIOPinWrite(GPIO_PORTF_BASE, (1 << (i + 1)), 0x00);
-            } else {
-                GPIOPinWrite(GPIO_PORTF_BASE, (1 << (i + 1)), (1 << (i + 1)));
+            if (i < 2) { // para los primeros dos espacios de estacionamiento (PF1 - PF4)
+                if (parkingStatus) {
+                    GPIOPinWrite(GPIO_PORTF_BASE, (1 << (2*i + 1)), 0x00); // LED verde
+                    GPIOPinWrite(GPIO_PORTF_BASE, (1 << (2*i + 2)), (1 << (2*i + 2))); // LED rojo
+                } else {
+                    GPIOPinWrite(GPIO_PORTF_BASE, (1 << (2*i + 1)), (1 << (2*i + 1))); // LED verde
+                    GPIOPinWrite(GPIO_PORTF_BASE, (1 << (2*i + 2)), 0x00); // LED rojo
+                }
+            } else { // para los últimos dos espacios de estacionamiento (PD0 - PD3)
+                if (parkingStatus) {
+                    GPIOPinWrite(GPIO_PORTD_BASE, (1 << (2*(i - 2))), 0x00); // LED verde
+                    GPIOPinWrite(GPIO_PORTD_BASE, (1 << (2*(i - 2) + 1)), (1 << (2*(i - 2) + 1))); // LED rojo
+                } else {
+                    GPIOPinWrite(GPIO_PORTD_BASE, (1 << (2*(i - 2))), (1 << (2*(i - 2)))); // LED verde
+                    GPIOPinWrite(GPIO_PORTD_BASE, (1 << (2*(i - 2) + 1)), 0x00); // LED rojo
+                }
             }
-        }
-
-        // Si el número de espacios disponibles ha cambiado, envía el nuevo valor a través de UART
-        if (availableSpaces != prevAvailableSpaces) {
-            UARTCharPut(UART0_BASE, availableSpaces); // Envía el número de espacios disponibles a través de UART
-            prevAvailableSpaces = availableSpaces; // Actualiza el número previo de espacios disponibles
         }
     }
 }
